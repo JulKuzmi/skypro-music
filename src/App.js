a792a5a88e946868f354e3d9ac943361fbf59f58
JulKuzmi <enchantress000@mail.ru>
2023-07-30 18:27:34 +0300

    add nav menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { AudioPlayer } from "./components/audioPlayer";
+import { Nav } from "./components/navMenu";
 function App() {
   return (
     <body>
       <div className="wrapper">
         <div className="container">
           <main className="main">
+            <Nav />
             <AudioPlayer />
           </main>
           <footer className="footer"></footer>
