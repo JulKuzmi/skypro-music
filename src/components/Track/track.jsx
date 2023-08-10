83cea0c59db210a6ffe26a2c7b5fb1ddda49aa9f
JulKuzmi <enchantress000@mail.ru>
2023-08-10 21:17:55 +0300

    add styled components

--- src/components/Track/track.jsx
+++ src/components/Track/track.jsx
@@ -1,40 +1,37 @@
 import { songs } from "../songs";
+import * as S from "./sceleton.style";
+
 export const Track = () => {
   return songs.map((song, index) => {
     return (
-      <div key={index} className="playlist__item">
-        <div className="playlist__track track">
-          <div className="track__title">
-            <div className="track__title-image">
-              <svg className="track__title-svg" alt="music">
+      <S.PlaylistItem>
+        <S.PlaylistTrack>
+          <S.TrackTitle>
+            <S.TrackTitleImage>
+              <S.TrackTitleSvg alt="music">
                 <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-              </svg>
-            </div>
-            <div className="track__title-text">
-              <a className="track__title-link" href="http://">
-                {song.name}{" "}
-                <span className="track__title-span">{song.add}</span>
-              </a>
-            </div>
-          </div>
-          <div className="track__author">
-            <a className="track__author-link" href="http://">
-              {song.author}
-            </a>
-          </div>
-          <div className="track__album">
-            <a className="track__album-link" href="http://">
-              {song.album}
-            </a>
-          </div>
-          <div className="track__time">
-            <svg className="track__time-svg" alt="time">
+              </S.TrackTitleSvg>
+            </S.TrackTitleImage>
+            <S.TrackAuthor>
+              <S.TrackTitleLink href="http://">
+                {song.name} <S.TrackTitleSpan>{song.add}</S.TrackTitleSpan>
+              </S.TrackTitleLink>
+            </S.TrackAuthor>
+          </S.TrackTitle>
+          <S.TrackAuthor>
+            <S.TrackAuthorLink href="http://">{song.author}</S.TrackAuthorLink>
+          </S.TrackAuthor>
+          <S.TrackAlbum>
+            <S.TrackAlbumLink href="http://">{song.album}</S.TrackAlbumLink>
+          </S.TrackAlbum>
+          <S.TrackTime>
+            <S.TrackTimeSvg alt="time">
               <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-            </svg>
-            <span className="track__time-text">{song.time}</span>
-          </div>
-        </div>
-      </div>
+            </S.TrackTimeSvg>
+            <S.TrackTimeText>{song.time}</S.TrackTimeText>
+          </S.TrackTime>
+        </S.PlaylistTrack>
+      </S.PlaylistItem>
     );
   });
 };
--- src/components/TrackList/trackList.jsx
+++ src/components/TrackList/trackList.jsx
@@ -2,6 +2,8 @@ import { Track } from "../Track/track";
 import { useEffect, useState } from "react";
 import { SceletonCard } from "../Track/sceleton";
 import { Filter } from "../Filter/filter";
+import * as S from "./trackList.style";
+
 export function TrackList() {
   const [isLoading, setLoading] = useState([]);
   const [songs, setPosts] = useState(false);
@@ -12,37 +14,32 @@ export function TrackList() {
       setLoading(false);
     }, 5000);
     return () => clearTimeout(time);
-  }, [songs]);
+  }, []);
   return (
-    <div className="main__centerblock centerblock">
-      <div className="centerblock__search search">
-        <svg className="search__svg">
+    <S.MainCenterblock>
+      <S.CenterblockSearch>
+        <S.SearchSvg>
           <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-        </svg>
-        <input
-          className="search__text"
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
-      <h2 className="centerblock__h2">Треки</h2>
+        </S.SearchSvg>
+        <S.SearchText type="search" placeholder="Поиск" name="search" />
+      </S.CenterblockSearch>
+      <S.CenterblockH2>Треки</S.CenterblockH2>
       <Filter />
-      <div className="centerblock__content">
-        <div className="content__title playlist-title">
-          <div className="playlist-title__col col01">Трек</div>
-          <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-          <div className="playlist-title__col col03">АЛЬБОМ</div>
-          <div className="playlist-title__col col04">
-            <svg className="playlist-title__svg" alt="time">
+      <S.CenterblockContent>
+        <S.ContentTitle>
+          <S.PlaylistTitleCol1>Трек</S.PlaylistTitleCol1>
+          <S.PlaylistTitleCol2>ИСПОЛНИТЕЛЬ</S.PlaylistTitleCol2>
+          <S.PlaylistTitleCol3>АЛЬБОМ</S.PlaylistTitleCol3>
+          <S.PlaylistTitleCol4>
+            <S.PlaylistTitleSvg alt="time">
               <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-            </svg>
-          </div>
-        </div>
-        <div className="content__playlist playlist">
+            </S.PlaylistTitleSvg>
+          </S.PlaylistTitleCol4>
+        </S.ContentTitle>
+        <S.ContentPlaylist>
           {isLoading ? <SceletonCard /> : <Track />}
-        </div>
-      </div>
-    </div>
+        </S.ContentPlaylist>
+      </S.CenterblockContent>
+    </S.MainCenterblock>
   );
 }
