d0003f30ca84f56cee30ab023b54e60f64b96fbc
JulKuzmi <enchantress000@mail.ru>
2023-08-14 19:21:43 +0300

    add registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import * as S from "./components/Main/App.style";
+import { Login } from "./pages/login";
 import { AppRoutes } from "./routes";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const handleLogin = () => setUser({ login: "token" });
+  const handleLogOut = () => setUser(null);
+
   return (
     <body>
       <S.GlobalStyle />
       <S.Wrapper>
-        <AppRoutes />
+        <S.Container>
+          <Login
+            user={user}
+            onAuthButtonClick={user ? handleLogOut : handleLogin}
+          />
+          <AppRoutes user={user} />
+          <footer></footer>
+        </S.Container>
       </S.Wrapper>
     </body>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login";
-export const AppRoutes = () => {
+import { Registration } from "./pages/registration";
+import { Main } from "./components/Main/Main";
+import { ProtectedRoute } from "./components/protector-route";
+import { NotFound } from "./pages/notFound";
+
+export const AppRoutes = ({ user }) => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route element={<ProtectedRoute isAllowed={Boolean(user)} />}>
+        <Route path="/main" element={<Main />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
