04eec044ca3c8f14950aed5c5abb6d9655450d58
JulKuzmi <enchantress000@mail.ru>
2023-09-14 20:25:10 +0300

    new api

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,59 +1,59 @@
+const host = "https://skypro-music-api.skyeng.tech/";
+let url = "";
 export async function getPlaylist() {
-  const response = await fetch(
-    "https://skypro-music-api.skyeng.tech/catalog/track/all/"
-  ).catch((error) => {
-    throw new Error("Не удалось загрузить плейлист, попробуйте позже");
-  });
-
-  const data = await response.json();
-  return data;
+  url = "catalog/track/all/";
+  return fetch(host + url, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else throw new Error();
+    })
+    .then((json) => json);
 }
 
 export async function loginUser({ email, password }) {
-  const response = await fetch(
-    "https://skypro-music-api.skyeng.tech/user/login/",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-      headers: {
-        "content-type": "application/json",
-      },
+  url = "user/login/";
+  return fetch(host + url, {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json();
     }
-  );
-  if (response.status === 401) {
-    throw new Error(
-      "Пользователь с таким email или паролем не найден, попробуйте заново"
-    );
-  } else if (response.status === 500) {
-    throw new Error("Сервер упал");
-  }
-  const data = await response.json();
-  return data;
+    if (response.status === 401) {
+      throw new Error("Пользователь с таким email или паролем не найден");
+    }
+    throw new Error("Ошибка,попробуйте заново...");
+  });
 }
 
 export async function registerUser({ email, password }) {
-  const response = await fetch(
-    "https://skypro-music-api.skyeng.tech/user/signup/",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password,
-        username: email,
-      }),
-      headers: {
-        "content-type": "application/json",
-      },
+  url = "user/signup/";
+  return fetch(host + url, {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      username: email,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status === 201) {
+      return response.json();
     }
-  );
-  if (response.status === 400) {
-    throw new Error("Такой пользователь уже существует");
-  } else if (response.status === 500) {
-    throw new Error("Сервер упал");
-  }
-  const data = await response.json();
-  return data;
+    if (response.status === 400) {
+      throw new Error("Пользователь с таким именем уже существует");
+    }
+    throw new Error("Ошибка,поробуйте заново...");
+  });
 }
