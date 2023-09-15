882aada8e224ceaa5328ebc9e071bd8f9215a302
JulKuzmi <enchantress000@mail.ru>
2023-09-15 22:10:10 +0300

    fixed API

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,19 +1,27 @@
 const host = "https://skypro-music-api.skyeng.tech/";
 let url = "";
-export async function getPlaylist() {
-  url = "catalog/track/all/";
-  return fetch(host + url, {
-    method: "GET",
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else throw new Error();
-    })
-    .then((json) => json);
-}
 
-export async function loginUser({ email, password }) {
+export const getAccessToken = async (email, password) => {
+  url = "user/token/";
+  return fetch(host + url, {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+
+    throw new Error("Ошибка,попробуйте заново...");
+  });
+};
+
+export const loginUser = async (email, password) => {
   url = "user/login/";
   return fetch(host + url, {
     method: "POST",
@@ -31,11 +39,12 @@ export async function loginUser({ email, password }) {
     if (response.status === 401) {
       throw new Error("Пользователь с таким email или паролем не найден");
     }
+
     throw new Error("Ошибка,попробуйте заново...");
   });
-}
+};
 
-export async function registerUser({ email, password }) {
+export const registerUser = async (email, password) => {
   url = "user/signup/";
   return fetch(host + url, {
     method: "POST",
@@ -54,6 +63,20 @@ export async function registerUser({ email, password }) {
     if (response.status === 400) {
       throw new Error("Пользователь с таким именем уже существует");
     }
-    throw new Error("Ошибка,поробуйте заново...");
+
+    throw new Error("Ошибка...");
   });
+};
+
+export async function getPlaylist() {
+  url = "catalog/track/all/";
+  return fetch(host + url, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else throw new Error();
+    })
+    .then((json) => json);
 }
